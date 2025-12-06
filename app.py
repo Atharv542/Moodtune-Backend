399287064206776ddd01a9aa688b7926deb28092
Atharv542 <atharvragdwal05@gmail.com>
2025-12-06 12:06:29 +0530

    Added changes

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ SPOTIFY_CLIENT_SECRET = "a66a684a4b5a4cfc8d73f97636f49d9e"
 # App init
 # ---------------------------
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+from flask_cors import CORS
+
+CORS(app, origins=["https://mood-tune-nine.vercel.app"])
+
 
 
 # ---------------------------
